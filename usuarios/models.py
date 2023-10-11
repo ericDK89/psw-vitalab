82671ab6d01767cac3a4629463cb4279addfd3eb
ericDK89 <ericthr42@gmail.com>
2023-10-11 16:15:42 -0300

    add: user auth

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, unique=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.username
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.contrib.messages import constants
+from django.contrib import messages
+from . import models
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 
 def cadastro(req):
     if req.method == 'GET':
         return render(req, 'cadastro.html')
+
     elif req.method == 'POST':
         primeiro_nome = req.POST.get('primeiro_nome')
         ultimo_nome = req.POST.get('ultimo_nome')
@@ -15,13 +19,19 @@ def cadastro(req):
         confirmar_senha = req.POST.get('confirmar_senha')
 
         if not senha == confirmar_senha:
-            return redirect('/usuarios/cadastro')
+            messages.add_message(req, constants.ERROR,
+                                 'As senhas não são iguais.')
 
         if len(senha) < 6:
+            messages.add_message(req, constants.ERROR,
+                                 'A senha precisa ter 7 ou mais digitos.')
+
+        if User.objects.filter(username=username).exists():
+            messages.add_message(req, constants.ERROR,
+                                 'Username já cadastrado.')
             return redirect('/usuarios/cadastro')
 
         try:
-            # TODO: validar se o username não existe
             user = User.objects.create_user(
                 username=username,
                 password=senha,
@@ -29,9 +39,34 @@ def cadastro(req):
                 first_name=primeiro_nome,
                 last_name=ultimo_nome
             )
+
+            user.save()
+
+            messages.add_message(req, constants.SUCCESS,
+                                 'Usuário cadastrado com sucesso.')
+
         except:
+            messages.add_message(req, constants.ERROR,
+                                 'Erro ao cadastrar usuário.')
             return redirect('/usuarios/cadastro')
 
-        user.save()
-
         return redirect('/usuarios/cadastro')
+
+
+def logar(req):
+    if req.method == 'GET':
+        return render(req, 'login.html')
+
+    elif req.method == 'POST':
+        username = req.POST.get('username')
+        senha = req.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login(req, user)
+            return redirect('/')
+        else:
+            messages.add_message(req, constants.ERROR,
+                                 'Usuário ou senha inválidos.')
+            return redirect('/usuarios/login')
